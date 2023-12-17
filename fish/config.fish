057e75b55ee616b0d92eddf0a552d26634a0b2f7
Yoann Chaudet <c.yoann@gmail.com>
2023-12-16 21:24:45 -0800

    Support both Apple silicon and intel

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
 if status --is-interactive
 
   # Setup brew (bin and co)
-  eval $(/opt/homebrew/bin/brew shellenv)
+  set brew_location /opt/homebrew/bin/brew
+  if not test -e "$brew_location"
+    set brew_location /usr/local/bin/brew
+  end
+  eval $($brew_location shellenv)
 
   # Start SSH-AGENT (if not done already)
   fish_ssh_agent
--- fish/make_default.sh
+++ fish/make_default.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Location where Brew install Fish
+# Location where Brew installs Fish (/opt/homebrew for Apple silicone and /usr/local for Intel)
 shell_location="/opt/homebrew/bin/fish"
+if [ ! -f "$shell_location" ]; then
+  shell_location="/usr/local/bin/fish"
+fi
 
 # If needed
 if ! grep -q "$shell_location" /etc/shells; then
@@ -9,5 +12,5 @@ if ! grep -q "$shell_location" /etc/shells; then
   echo "$shell_location" | sudo tee -a /etc/shells
 
   # Make it the default one (or "login shell")
-  chsh -s /opt/homebrew/bin/fish
+  chsh -s "$shell_location"
 fi
