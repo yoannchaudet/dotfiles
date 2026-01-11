67c7467b500f2b3249d0eb181b84361f9b7b22e5
Yoann Chaudet <c.yoann@gmail.com>
2026-01-11 09:48:04 -0800

    remove iterm2 and swap by ghostty

--- iterm2/set_settings.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Change the settings folder
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string ~/.config/iterm2
-
-# Load the settings from the folder
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
